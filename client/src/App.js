760116d5c1ac6dab3464851c84904e7ab8f27077
Richard Durazo <rcdurazo@gmail.com>
2018-08-03 12:56:53 -0700

    add common from components and create profile view add protected route for dashboard and create profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'; 
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
@@ -7,6 +7,7 @@ import { setCurrentUser, logoutUser } from './actions/authActions';
 import { Provider } from 'react-redux';
 import store from './store';
 
+import PrivateRoute from './components/common/PrivateRoute'
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer.jsx';
 import Landing from './components/layout/Landing';
@@ -15,6 +16,7 @@ import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import './App.css';
 import { clearCurrentProfile } from './actions/profileActions';
+import CreateProfile from './components/create-profile/CreateProfile';
 
 // check for token 
 if (localStorage.jwtToken) {
@@ -47,7 +49,12 @@ class App extends Component {
               <div className="container">
                 <Route exact path="/register" component={Register} />
                 <Route exact path="/login" component={Login} />
-                <Route exact path="/dashboard" component={Dashboard} />
+                <Switch>
+                  <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                </Switch>
+                <Switch>
+                  <PrivateRoute exact path="/create-profile" component={CreateProfile} />
+                </Switch>
               </div>
             <Footer />
           </div>
